0d0c623852e6e21457bb330bd4ab358ec0691f6c
NghiemLe <nghiemlehong98@gmail.com>
2021-03-04 14:51:42 +0700

    better

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
 const express = require('express')
+const app = express()
+const PORT = 9999 || process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('<h1>Le Hong Nghiem</h1>')
+})
+
+app.listen(PORT, () => console.log('SERVER STARTED'))
